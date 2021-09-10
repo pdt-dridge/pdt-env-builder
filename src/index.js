4ccec286983de8e74d7d653aeea2b15a02118a56
pdt-dridge <dridge@pagerduty.com>
2021-09-10 18:53:14 +1000

    updating for github pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Container, Header } from "semantic-ui-react";
-
-import pkg from 'semantic-ui-react/package.json'
 import EnvBuilderForm from "./EnvBuilderForm";
 
 const App = ({ children }) => (
